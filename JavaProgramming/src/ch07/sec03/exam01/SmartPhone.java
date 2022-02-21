f492df8c996fcb296628303e9999650a8717ef0b
byuns <bsj0887@daum.net>
2022-02-21 16:31:03 +0900

    2022-02-21

---
+++ JavaProgramming/src/ch07/sec03/exam01/SmartPhone.java
@@ -0,0 +1,12 @@
+package ch07.sec03.exam01;
+
+public class SmartPhone extends Phone{
+	public SmartPhone(String owner) {
+		super(owner);
+	}
+	
+	public void InternetSearch() {
+		System.out.println("인터넷 검색을 합니다.");
+	}
+
+}
---
+++ JavaProgramming/src/ch07/sec03/exam03/HttpServlet.java
@@ -0,0 +1,5 @@
+package ch07.sec03.exam03;
+
+public abstract class HttpServlet {
+	public abstract void service();
+}
