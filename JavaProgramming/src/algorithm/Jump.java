9d30e5a039430f0a063c1593c7343a585b4ca7f3
byuns <bsj0887@daum.net>
2022-02-23 12:30:58 +0900

    2022-02-23

--- JavaProgramming/src/algorithm/Jump.java
+++ JavaProgramming/src/algorithm/Jump.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class Jump {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		int length = Integer.parseInt(sc.nextLine());
-		String[][] board = new String[length][];
-		int[][] dp = new int[101][101];
+		String[][] board = new String[length][length];
+		int[][] dp = new int[length+1][length+1];//최대 길이 = 받은 길이 + 1
 		
-		dp[0][0] = 1;
+		dp[0][0] = 1; // 경로 수 저장
 		
 		for(int i=0; i<board.length; i++) { // 게임 판에 값 저장
 			board[i] = sc.nextLine().split(" ");
@@ -18,24 +17,24 @@ public class Jump {
 		
 		for(int i = 0; i<board.length; i++) {			
 			for(int j = 0; j < board.length; j++) {
-				if(i == board.length-1 && j == board.length-1) {
+				if(i == length-1 && j == length-1) { //끝에 도달하는 것은 제외
 					continue;
 				}
-				int dist = Integer.parseInt(board[i][j]);
-				int down = dist + i;
-				int right = dist + j;
+				int tmp = Integer.parseInt(board[i][j]); 
+				int down = tmp + i; // 세로 경로를 저장
+				int right = tmp + j; // 가로 경로를 저장
 				
 				if(down < board.length) {
-					dp[down][j] += dp[i][j];
+					dp[down][j] += dp[i][j]; // dp에 세로 경로를 하나씩 추가
 				}
 				if(right < board.length) {
-					dp[i][right] += dp[i][j];
+					dp[i][right] += dp[i][j]; // dp에 가로 경로를 하나씩 추가
 				}
 
 			}
 
 		}
-		System.out.println(dp[length-1][length-1]);
+		System.out.println(dp[length-1][length-1]); // 최종 경로를 출력
 		
 		
 	}
