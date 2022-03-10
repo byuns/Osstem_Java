0a7f6009ff191f3a9aa9096c5297d61bdf294744
byuns <bsj0887@daum.net>
2022-03-10 15:15:28 +0900

    2022-03-10

--- jdbcprogramming/src/exam02_select/SelectExample06.java
+++ jdbcprogramming/src/exam02_select/SelectExample06.java
@@ -13,83 +13,61 @@ public class SelectExample06 {
 
 	public static void main(String[] args) {
 		
+	
 		Scanner sc = new Scanner(System.in);
+		System.out.print("이메일 : ");
+		String email = sc.nextLine();
+		System.out.print("비밀번호 : ");
+		String password = sc.nextLine();
 		
-		System.out.print("사번 : ");
-		String employeeId = sc.nextLine();
-
-		
-		List<Employee> list = getList(employeeId);
-		for(Employee e : list) {
-			System.out.print("employeeId : " +e.getEmployeeId()+", ");
-			System.out.print("firstName : " +e.getFirstName()+", ");
-			System.out.print("lastName : " +e.getLastName()+", ");
-			System.out.print("email : " +e.getEmail()+", ");
-			System.out.print("phoneNumber : " +e.getPhoneNumber()+", ");
-			System.out.print("hireDate : " +e.getHireDate()+", ");
-			System.out.print("jobId : " +e.getJobId()+", ");
-			System.out.print("salary : " +e.getSalary()+", ");
-			System.out.print("commissionPct : " +e.getCommissionPct()+", ");
-			System.out.print("managerId : " +e.getManagerId()+", ");
-			System.out.print("departmentId : " +e.getDepartmentId()+"\n");
+		//로그인 처리
+		String result = login(email,password);
+		if(result.equals("success")) {
+			System.out.println("로그인 성공");
+		}else if(result.equals("wrongEmail")) {
+			System.out.println("이메일이 존재하지 않습니다.");
+		}else if(result.equals("wrongPassword")) {
+			System.out.println("비밀번호가 틀립니다.");
 		}
-	}	
-		
-		public static List<Employee> getList(String employeeId) {
-		Connection conn = null;
-		List<Employee> list = new ArrayList();
+	}
+	
+	public static String login(String email, String password) {
+		Connection con = null;
+		String result = null;
 		try {
-			//연결하기
 			Class.forName("oracle.jdbc.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa1.iptime.org:50102/orcl","hr","kosa12345");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@kosa1.iptime.org:50102/orcl","hr","kosa12345");
 			System.out.println("연결 성공");
-			//실행해야할 SQL 작성
-
-			String sql = "select * from employees where employee_id = ?";
-//			System.out.println(sql);
-			// SQL를 실행할 PrepareStatement 얻기
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, employeeId );
 			
-			// SQL을 실행해서 결과 얻기
+			String sql = "select first_name from employees where email = ?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setString(1,email);
 			ResultSet rs = pstmt.executeQuery();
 			
-			
-			while(rs.next()) {
-				Employee em = new Employee();
-				
-				em.setEmployeeId(rs.getInt("employee_id"));
-				em.setFirstName(rs.getString("first_name"));
-				em.setLastName(rs.getString("last_name"));
-				em.setEmail(rs.getString("email"));
-				em.setPhoneNumber(rs.getString("phone_number"));
-				em.setHireDate(rs.getDate("hire_date"));
-				em.setJobId(rs.getString("job_id"));
-				em.setSalary(rs.getDouble("salary"));
-				em.setCommissionPct(rs.getDouble("commission_pct"));
-				em.setManagerId(rs.getInt("manager_id"));
-				em.setDepartmentId(rs.getInt("department_id"));
-
-				list.add(em);
-			}
-			//위에서 사용한 메모리 해제
-			rs.close();
-			pstmt.close();
-			
-			}catch(ClassNotFoundException e) {
-				e.printStackTrace();
-			}catch(SQLException e) {
-				e.printStackTrace();
-			}finally {
-				//연결 끊기
-				try {
-					conn.close();
-					System.out.println("연결 끊김");
-				} catch (SQLException e) {
+			if(rs.next()) {
+				String pw = rs.getString("first_name");
+				if(pw.equals(password)) {
+					result = "success";
+				}else {
+					result = "wrongPassword";
 				}
+			}else {
+				result = "wrongEmail";
 			}
-		return list;
-		
+			
+			
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			//연결 끊기
+			try {
+				con.close();
+				System.out.println("연결 끊김");
+			} catch (SQLException e) {
+			}
+		}
+		return result;
 	}
-
 }
